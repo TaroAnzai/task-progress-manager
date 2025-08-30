6245172dc3689b5e593e90d184623ae5efef4838
Taro Anzai <taro.anzai@gmail.com>
2025-08-30 23:32:31 +0900

    Fix bug in orval config

--- orval.config.ts
+++ orval.config.ts
@@ -1,11 +1,6 @@
 import { config } from 'dotenv';
-import * as path from 'path';
 
-// NODE_ENV に応じて環境ファイルを切り替え
-const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development';
-
-config({ path: path.resolve(process.cwd(), envFile) });
-
+config({ path: '.env.production' });
 export default {
   progressApi: {
     input: process.env.VITE_OPENAPI_URL,
